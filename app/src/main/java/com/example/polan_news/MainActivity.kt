c848b12c45346fe2da75620fbb2fb2e6006acf73
Utkarsh SInha <vishalsingh@Vishals-MacBook-Air.local>
2023-09-28 19:07:49 +0530

    signup form done

--- app/src/main/java/com/example/polan_news/MainActivity.kt
+++ app/src/main/java/com/example/polan_news/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.polan_news
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val btn = findViewById<Button>(R.id.clickme)
+
+        btn.setOnClickListener {
+            val intent = Intent(this,signUpFormActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/polan_news/signUpFormActivity.kt
@@ -0,0 +1,11 @@
+package com.example.polan_news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class signUpFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_form)
+    }
+}
